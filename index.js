93505fdae749ec8b104764d23f07ce5861ebaa36
Ryan Marcus <ryan@rmarcus.info>
2015-11-02 00:01:10 -0500

    added license

--- index.js
+++ index.js
@@ -1,13 +1,26 @@
 #! /usr/bin/env node
 
+
+//This file is part of skill-calendar.
+// 
+//skill-calendar is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+// 
+//skill-calendar is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	 See the
+//GNU General Public License for more details.
+// 
+//You should have received a copy of the GNU General Public License
+//along with skill-calendar.  If not, see <http://www.gnu.org/licenses/>.
+
+
 "use strict";
 
-var FS = require("q-io/fs");
 var Q = require("q");
 var gen = require("./gen.js");
-
-
-
 
 function optAsPromise(txt) {
 	var svgo = require('svgo');
